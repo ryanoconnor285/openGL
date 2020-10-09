e5a83fdc9aade79105c85552accdda255f119d27
Ryan OConnor <ryanoconnor@Ryans-MacBook-Pro.local>
2020-10-08 20:39:03 -0400

    initial commit

---
+++ Modern OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Modern OpenGL
+//
+//  Created by Ryan OConnor on 10/8/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
